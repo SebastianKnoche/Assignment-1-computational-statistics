e7c0a997a26fa498c947b4c2cd52729917e61870
Sebastian Knoche <sebastianknoche@protonmail.com>
2021-05-15 16:02:26 +0200

    Revert "Functions for assignment 2.1-2.3"

--- assignment_1_function_lib.R
+++ assignment_1_function_lib.R
@@ -5,7 +5,7 @@
 
 # simulate rolling the die a few thousand times (e.g. 10000)
 # estimate the expectation and the variance from this simulation data
-sim_die <- function(n){
+sim_die <- function(n=10000){
   tmp_die <- sample(1:6, n, replace = TRUE)
   #expectation/mean
   e <- sum(tmp_die)/n
@@ -17,7 +17,7 @@ sim_die <- function(n){
 
 # compute the average of 20 rolls several thousand times
 # and estimates the expectation and the variance of $\overline{Y}$
-sim_av20_die <- function(n){
+sim_av20_die <- function(n=10000){
   tmp_e <- c()
   tmp_v <- c()
   for(i in 1:n){
@@ -40,8 +40,8 @@ sim_av20_die <- function(n){
 # x <- rnorm(1000,mean=10,sd=3)
 
 #compute a single t statistic
-my.tstat <- function(x,mu0) {
-  (mean(x) -mu0)/sd(x)*sqrt(length(x))
+my.tstat <- function(x,mu0=8) {
+  #
 } #note H_0 is false here
 
 
@@ -56,12 +56,8 @@ my.tstat <- function(x,mu0) {
 # The number of rows is equal to `nReps`.
 
 #generate a matrix of random variables
-my.genSampleMatrix <- function(nReps, nSample, mu, sigma) {
-  SampleMatrix <- matrix(nrow = nReps,ncol=nSample,byrow = TRUE) #Create Empty Matrix with nReps Rows and nSize Columns
-  for (row in seq(nrow(SampleMatrix))){ 
-    SampleMatrix[row,] <- rnorm(nSample,mean = mu,sd=sigma^2) #Fill out Matrix by Row with each sample
-  }
-  return(SampleMatrix) #Output Result
+my.genSampleMatrix <- function(nReps=2, nSample=5, mu=2, sigma=2) {
+  #
 }
 
 
@@ -75,13 +71,8 @@ my.genSampleMatrix <- function(nReps, nSample, mu, sigma) {
 # 2) `mu0`: the value according to the Null hypothesis.
 
 #compute vector of t stats
-my.compute.tVector <- function(sampleMatrix, mu0) {
-     apply(
-       sampleMatrix, # Matrix to apply
-       MARGIN = 1, #Set to apply by row
-       my.tstat, #function to apply
-       mu0 = mu0 #extra argument for funtion my.tstat 
-    ) 
+my.compute.tVector <- function(sampleMatrix=my.samMatrix, mu0=2) {
+  #
 }
 
 
